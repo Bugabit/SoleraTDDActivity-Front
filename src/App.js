f6fc0644f04a62d59045c227d9e4b2496ef807df
Javi <javisr997@gmail.com>
2022-11-18 15:53:21 +0100

    Frontend commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import OriginList from "./components/OriginList";
+import DestinationList from "./components/DestinationList";
+import APIService from "./services/APIService";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [origins, setOrigins] = useState([]);
+
+  useEffect(() => {
+    APIService.getOrigins()
+      .then(({ data }) => {
+        setOrigins(data);
+      })
+      .catch(function (ex) {
+        console.log("Response parsing failed. Error: ", ex);
+      });
+  }, []);
+
+  if(origins.length === 0) 
+  return null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<OriginList origins={origins} />} />
+          {origins.map(({id, destinations}) => 
+            <Route path={`origin/${id}`} element={<DestinationList destinations={destinations} />} />
+          )}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
